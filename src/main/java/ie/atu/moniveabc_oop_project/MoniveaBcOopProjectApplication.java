65781debab5c006c49e865531e480795f87db404
FionanMcC <fionanmccrossan@gmail.com>
2026-03-11 14:44:20 +0000

    signUp service and user model added

---
+++ src/main/java/ie/atu/moniveabc_oop_project/MoniveaBcOopProjectApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.moniveabc_oop_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoniveaBcOopProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoniveaBcOopProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/moniveabc_oop_project/MoniveaBcOopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.moniveabc_oop_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoniveaBcOopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
