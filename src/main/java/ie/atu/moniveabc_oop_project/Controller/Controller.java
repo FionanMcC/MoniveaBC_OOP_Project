2070bd6dfbcb316588d85a136b8e74eecc7e2b35
Evan Farrell <g00326940@atu.ie>
2026-03-25 13:48:42 +0000

    Controller created

---
+++ src/main/java/ie/atu/moniveabc_oop_project/Controller/Controller.java
@@ -0,0 +1,37 @@
+package ie.atu.moniveabc_oop_project.Controller;
+
+import ie.atu.moniveabc_oop_project.Model.userModlel;
+
+import ie.atu.moniveabc_oop_project.Service.signUpService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/users")
+public class Controller {
+
+    private final signUpService signUpService;
+
+    public Controller(signUpService signUpService) {
+        this.signUpService = signUpService;
+    }
+
+
+    @PostMapping("/register")
+    public String registerUser(@Valid @RequestBody userModlel user) {
+        return signUpService.registerUser(user);
+    }
+
+    @GetMapping
+    public List<userModlel> displayUsers() {
+        return signUpService.getUsers();
+    }
+
+}
