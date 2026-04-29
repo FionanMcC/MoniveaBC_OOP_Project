03aaa9a5bc01d58dc568a474667b5bf10700e765
FionanMcC <fionanmccrossan@gmail.com>
2026-04-29 17:21:37 +0100

    Updated Controller with get by id, update and delete

--- src/main/java/ie/atu/moniveabc_oop_project/Controller/Controller.java
+++ src/main/java/ie/atu/moniveabc_oop_project/Controller/Controller.java
@@ -4,11 +4,7 @@ import ie.atu.moniveabc_oop_project.Model.UserModel;
 
 import ie.atu.moniveabc_oop_project.Service.SignUpService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +29,20 @@ public class Controller {
     public List<UserModel> displayMembers() {
         return signUpService.getUser();
     }
+    @GetMapping("/{id}")
+    public UserModel getMember(@PathVariable Long id) {
+        return signUpService.getById(id);
+    }
 
+    @PutMapping("/{id}")
+    public UserModel updateMember(@PathVariable Long id,
+                                  @RequestBody UserModel member) {
+        return signUpService.updateUser(id, member);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteMember(@PathVariable Long id) {
+        signUpService.deleteUser(id);
+        return "Member deleted successfully";
+    }
 }
